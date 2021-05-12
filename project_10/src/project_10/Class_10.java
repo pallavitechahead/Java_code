c0b1b40e0776e09ce2ded249ef0556bc019e3fb2
Pallavi Shahi <Pallavi Shahi@DESKTOP-TM8SAOD>
2021-05-12 23:53:09 +0530

    My first Commit.

---
+++ project_10/src/project_10/Class_10.java
@@ -0,0 +1,50 @@
+package project_10;
+
+public class Class_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Class_10 A = new Class_10();
+		 
+		 A.sum();
+		 A.sub();
+		 A.sum(100, 200, 30);
+	}
+		
+		public void sum()
+		{
+			
+			int a= 2;
+			int b =4;
+			int c =(a+ b);
+			
+			System.out.println(c);
+			
+		}
+
+
+		public void sub()
+		{
+			
+			int X= 2;
+			int Y =4;
+			int Z =(X-Y);
+			
+			System.out.println(Z);
+		
+		}	
+		
+		
+		public int sum(int s, int q, int f)
+		{
+			int z=(s+q+f);
+			System.out.println(z);
+			
+		return z;	
+		}
+		
+		
+	
+
+}
